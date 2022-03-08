208d79022ea040711886055010ce6512312b8239
陈宏庆 <chenhongqing@chenhongqingdeMacBook-Pro.local>
2022-03-08 11:02:00 +0800

    version-1.0.11

--- solox/__main__.py
+++ solox/__main__.py
@@ -33,7 +33,7 @@ def open_url():
 
 def start_web():
     """start solox web service"""
-    app.run(debug=True, host='0.0.0.0', port=5000,use_reloader=False)
+    app.run(debug=False, host='0.0.0.0', port=5000)
 
 
 def main():
