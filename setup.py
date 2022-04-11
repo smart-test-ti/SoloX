78896dee190b801a76db7bcbd7e31d007f15efc3
Lijiawei <1456470136@qq.com>
2022-04-11 22:22:45 +0800

    ci: format project
    1.移除打包产物
    2.增加.gitignore，便于提交项目
    3.标准化setup.py 版本号导入
    4.删除多余的requirement，依赖从setup.py获取

--- setup.py
+++ setup.py
@@ -4,14 +4,15 @@
 # Licensed under MIT
 #
 import setuptools
+from solox import __version__
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    install_requires=['flask>=2.0.1','requests','logzero','Flask-SocketIO==4.3.1',
-                      'python-engineio==3.13.2','python-socketio==4.6.0','Werkzeug==2.0.3','Jinja2==3.0.1'],
-    version='1.0.30',
+    install_requires=['flask>=2.0.1', 'requests', 'logzero', 'Flask-SocketIO==4.3.1',
+                      'python-engineio==3.13.2', 'python-socketio==4.6.0', 'Werkzeug==2.0.3', 'Jinja2==3.0.1'],
+    version=__version__,
     long_description=long_description,
     long_description_content_type="text/markdown",
     description="APP性能测试 - Simple test in SoloX",
