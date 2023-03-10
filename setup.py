36c9f2cbc18d02b2c14bf3fd4deb943da2052caa
chenhq <laoqi1988_f1@126.com>
2023-03-10 09:39:32 +0800

    Werkzeug == 2.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     install_requires=['flask>=2.0.1', 'requests', 'logzero', 'Flask-SocketIO==4.3.1', 'fire',
-                      'python-engineio==3.13.2', 'python-socketio==4.6.0', 'Werkzeug>=2.2.3',
+                      'python-engineio==3.13.2', 'python-socketio==4.6.0', 'Werkzeug==2.0.3',
                       'Jinja2==3.0.1','tidevice>=0.9.7', 'tqdm', 'xlwt'],
     version=__version__,
     long_description=long_description,
