1ee93509924a5d13ca489c2caa4699e4cab074ae
chenhq <laoqi1988_f1@126.com>
2023-07-17 09:22:21 +0800

    fix #181

--- solox/public/common.py
+++ solox/public/common.py
@@ -29,7 +29,7 @@ class Devices:
     def execCmd(self, cmd):
         """Execute the command to get the terminal print result"""
         r = os.popen(cmd)
-        text = r.buffer.read().decode(encoding='gbk').strip()
+        text = r.buffer.read().decode(encoding='gbk').replace('\x1b[0m','').strip()
         r.close()
         return text
 
