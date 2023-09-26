dfd0384b27671a0a446420320cd7a474c2a19786
chenhq <laoqi1988_f1@126.com>
2023-09-26 17:59:21 +0800

    update packages version

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     install_requires=['flask>=2.0.1', 'requests', 'logzero', 'Flask-SocketIO==4.3.1', 'fire',
-                      'python-engineio==3.13.2', 'python-socketio==4.6.0', 'Werkzeug==2.0.3',
-                      'Jinja2==3.0.1','tidevice==0.9.7', 'tqdm', 'xlwt','pyfiglet','psutil',
+                      'python-engineio==3.13.2', 'python-socketio==4.6.0', 'Werkzeug',
+                      'Jinja2','tidevice==0.9.7', 'tqdm', 'xlwt','pyfiglet','psutil',
                       'opencv-python'],
     version=__version__,
     long_description=long_description,
